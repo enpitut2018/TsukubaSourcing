5bd27543521b13814571b40d56e4914a5a7153d3
oshibori <oshibori0121vox@gmail.com>
2018-07-24 16:24:50 +0900

    postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2018_07_24_062027) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "assigns", force: :cascade do |t|
     t.integer "job_id"
     t.integer "user_id"
