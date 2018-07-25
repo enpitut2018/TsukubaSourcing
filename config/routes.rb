c2167db689f5a3c99fda5500e1b465a2b34c9839
N-Mattu <mattu.0227@gmail.com>
2018-07-25 01:00:37 +0000

    delete vendor

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :assigns
+  devise_for :users
   resources :jobs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to:"home#index"
 end
