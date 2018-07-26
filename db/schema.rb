4daf1cf6005fca869fa466141fabd3535bb9eb1f
ishowta <kinironote@gmail.com>
2018-07-27 07:25:48 +0900

    clean

--- db/schema.rb
+++ db/schema.rb
@@ -49,14 +49,6 @@ ActiveRecord::Schema.define(version: 2018_07_26_170104) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "jobs_evaluations", force: :cascade do |t|
-    t.text "comment"
-    t.integer "star"
-    t.integer "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
