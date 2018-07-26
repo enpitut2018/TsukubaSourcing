46f78f48bfb8723736a54adeb115ec8698e54ebc
ishowta <kinironote@gmail.com>
2018-07-26 11:08:35 +0900

    完了時にbackするようにredirectを変更

--- app/controllers/assigns_controller.rb
+++ app/controllers/assigns_controller.rb
@@ -42,7 +42,7 @@ class AssignsController < ApplicationController
   def update
     respond_to do |format|
       if @assign.update(assign_params)
-        format.html { redirect_to jobs_path, notice: 'Assign was successfully updated.' }
+        format.html { redirect_back fallback_location: root_path, notice: 'Assign was successfully updated.' }
         format.json { render :show, status: :ok, location: @assign }
       else
         format.html { render :edit }
