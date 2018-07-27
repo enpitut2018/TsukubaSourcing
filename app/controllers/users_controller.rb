6387aaa7a99558e597ad7a643f5ab0d4e150b2e6
ishowta <kinironote@gmail.com>
2018-07-27 16:50:45 +0900

    実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
   def show
+    user_id = params[:id]
+    @user = User.find(user_id)
+    @works = Job.where(id: @user.assigns.where.not(status: 'applying').select(:user_id))
+    @reviews = @user.reviews
   end
 end
